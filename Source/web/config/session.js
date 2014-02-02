6734c6eee031728e236d3e1d3fdf0ed262eab233
Teodor92 <teodor.ivanov92@gmail.com>
2014-02-02 20:29:53 +0200

    Added basic project structure. #3

--- Source/web/config/session.js
+++ Source/web/config/session.js
@@ -15,7 +15,7 @@ module.exports.session = {
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
   // forcing them to log in again. 
-  secret: 'dc3f76b8c39280606b961982afc83d50'
+  secret: '6bba2d2b4050f978e4cbdef8493accac'
 
 
   // In production, uncomment the following lines to set up a shared redis session store
